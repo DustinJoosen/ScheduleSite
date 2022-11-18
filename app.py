dfcf740f4be217efb54d074d75a89b482c607af7
DustinJoosen <dustinjoosen2003@gmail.com>
2022-11-19 00:35:40 +0100

    add a start to the templates

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app: Flask = Flask(__name__)
 
 
 @app.route('/')
 def index() -> str:
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/schedule')
+@app.route('/rooster')
+def schedule() -> str:
+    return render_template(
+        'schedule.html',
+        class_name='WFHBOICT22.VG'
+    )
 
 
 if __name__ == "__main__":
