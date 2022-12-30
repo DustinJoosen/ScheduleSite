eee812d94c8f6ca2e5bf7eccc5f6acacc37adbca
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2022-12-30 20:17:14 +0100

    Cookie modal (#4)
    * add cookie question modal
    
    * store cookie allowance

--- static/js/main.js
+++ static/js/main.js
@@ -39,20 +39,37 @@ zscb.onchange = function(e) {
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
+function get_cookie_by_name(name) {
+    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return (match == null) ? null : match[2];
+}
+
 // modifies the url, based on the cookies
-let match = document.cookie.match(new RegExp('(^| )' + "show_zelfstudie" + '=([^;]+)'));
-if (match) {
-    if (match[2] === "true") {
-        // we don't want to send you to zelfstudie=true if you are already there.
-        if (params.show_zelfstudie !== "true" && params.show_zelfstudie !== "yes") {
-            document.location.href = "?show_zelfstudie=true";
-        }
+let show_zelfstudie_cookie = get_cookie_by_name("show_zelfstudie");
+if (show_zelfstudie_cookie === "true") {
+    // we don't want to send you to zelfstudie=true if you are already there.
+    if (params.show_zelfstudie !== "true" && params.show_zelfstudie !== "yes") {
+        document.location.href = "?show_zelfstudie=true";
     }
+
 }
 
 // set the checked attribute on the checkbox
 if (params.show_zelfstudie === "true" || params.show_zelfstudie === "yes") {
     zscb.checked = true;
+}
+
+function are_cookies_allowed() {
+    let cookies_allowed = get_cookie_by_name("cookies_allowed");
+    if (cookies_allowed == null) {
+        return false;
+    }
+    return cookies_allowed === "true"
+}
+
+function set_cookies_allowed(allowed) {
+    let date_one_year_from_now = new Date(new Date().getTime() + 1000*3600*24*365).toGMTString();
+    document.cookie = "cookies_allowed=" + allowed + "; expires=" + date_one_year_from_now;
 }
 
 
@@ -68,3 +85,8 @@ $("#copy_raw_data").on("click", function () {
     alert("Raw JSON data is now copied to clipboard");
     navigator.clipboard.writeText("Deze functionaliteit is nog niet af.");
 });
+
+if (get_cookie_by_name("cookies_allowed") == null) {
+    $("#cookieModal").modal("show");
+}
+
