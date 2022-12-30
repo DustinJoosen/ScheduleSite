6220e0a85da975b56e7d7c81a496045c29c93917
DustinJoosen <dustinjoosen2003@gmail.com>
2022-12-30 23:37:41 +0100

    cache setup works

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 from flask import Flask, render_template, redirect, request
+from flask_caching import Cache, current_app
 from werkzeug import Response
 from datetime import datetime, timedelta
 from settings.settings import Settings
 from schedule.schedule import get_schedule
 
 app: Flask = Flask(__name__)
+cache: Cache = Cache(config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': '/tmp'})
+cache.init_app(app)
 
 Settings.load()
 
 
 @app.route('/', methods=['GET'])
 def schedule() -> Response | str:
-
+    # Handle the 'show_zelfstudie'.
     show_zelfstudie: bool = False
     if request.args.get('show_zelfstudie') in ['true', 'yes']:
         show_zelfstudie = True
 
-    schedule: dict = get_schedule(show_zelfstudie=show_zelfstudie)
+    # Send a request for the schedule, and try the cache.
+    with current_app.app_context():
+        schedule = cache.get("schedule")
+        print('schedule: ')
+        print(schedule)
+        if schedule is None:
+            schedule = get_schedule(show_zelfstudie=show_zelfstudie)
+            print('schedule out of cache was none. new schedule: ')
+            print(schedule)
+            cache.set("schedule", schedule)
+            print("new schedule set in cache")
+
+        schedule = cache.get("schedule")
+        if schedule is None:
+            raise Exception("Cached schedule is still None, Fuckhead")
+
+    # Handle unexpected schedule output.
     if error_msg := schedule.get("error"):
         return redirect(f'/error?e={error_msg}')
-
     if schedule is None:
         return redirect('/error')
 
