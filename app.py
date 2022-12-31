b396c32e4109dc7e4c3ec4c3de0007e6c611b1f9
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2022-12-31 14:11:39 +0100

    Cache schedule (#5)
    * cache setup works
    
    * cache applied to schedule retrieving
    
    * set cache expiricy at 10 minutes

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, render_template, redirect, request
+from flask_caching import Cache
 from werkzeug import Response
 from datetime import datetime, timedelta
 from settings.settings import Settings
 from schedule.schedule import get_schedule
 
 app: Flask = Flask(__name__)
+cache: Cache = Cache(app, config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': '/tmp',
+    'CACHE_DEFAULT_TIMEOUT': 600
+})
+cache.init_app(app)
 
 Settings.load()
 
 
 @app.route('/', methods=['GET'])
 def schedule() -> Response | str:
-
+    # Handle the 'show_zelfstudie'.
     show_zelfstudie: bool = False
     if request.args.get('show_zelfstudie') in ['true', 'yes']:
         show_zelfstudie = True
 
-    schedule: dict = get_schedule(show_zelfstudie=show_zelfstudie)
+    # Get the schedule. Cache is applied.
+    schedule = get_schedule(show_zelfstudie=show_zelfstudie, cache=cache)
+
+    # Handle unexpected schedule output.
     if error_msg := schedule.get("error"):
         return redirect(f'/error?e={error_msg}')
-
     if schedule is None:
         return redirect('/error')
 
